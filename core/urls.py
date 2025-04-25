1365063df33c49bb8f3c55e0a99ea9fbe4b6b424
tanthongtan <tan.thongtan1@gmail.com>
2025-04-25 16:26:59 +0700

    add core

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-app_name = 'prices'
+app_name = 'core'
 
 # The URLs are now determined automatically by the router.
 urlpatterns = [
+    path('hello-webpack/', TemplateView.as_view(template_name='core/hello_webpack.html')),
 ]
